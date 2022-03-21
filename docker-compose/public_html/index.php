e4e329b4bde3d23a1073947761e97bd2e85d3f8b
Felipe E. Ribeiro <75494201+felipe-e-ribeiro@users.noreply.github.com>
2022-03-20 23:16:53 -0300

    Update index.php

--- docker-compose/public_html/index.php
+++ docker-compose/public_html/index.php
@@ -6,7 +6,7 @@ Server <?php print exec('cat /run/hostname_server | awk -F \- \'{print $2}\' ');
 </title>
 <style>
 html { color-scheme: dark; }
-body { width: 35em; margin: 0 auto;
+body { width: 60em; margin: 0 auto;
 font-family: Tahoma, Verdana, Arial, sans-serif; }
 </style>
 </head>
@@ -33,7 +33,7 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
 <li> Layout de disco e partições</li>
 <ul>
 <li> Entender sobre consumo de disco</li>
-<li>Extra: Pesquisar sobre inodes.</li>
+<p>Extra: Pesquisar sobre inodes.</p>
 <li> Identificar um ponto de montagem e entender como que isto fica dentro de uma estrutura de diretórios;  </li>
 <li> Entender o processo de criação/formatação de um novo disco;                                            </li>
 <li> Entender alocação de espaço raiz x alocação de espaço unidade montada;                                 </li>
@@ -53,24 +53,23 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
 <li>Entender diferença do elemento "x" em pastas e arquivos.            </li>
 </ul>
 <ol>
-<p>----------------------------------------------------------------------------------------------------</p>
- <p><input type="checkbox">  Criar um novo disco com ao menos 10 MB; </p>
- <p><input type="checkbox">  OPCIONAL: Criar um lvm chamado lv_disco; </p>
- <p><input type="checkbox">  Formatar o novo disco com ext4 e montar em /mnt/teste_deu_boa; </p>
- <p># OBS: Este ponto de montagem deve ser iniciado após um reboot. </p>
+<hr>
+<p>DESAFIO 01</p>
+ <p><input type="checkbox">  Criar um novo disco com ao menos 10 MB. OPCIONAL: Criar um lvm chamado lv_disco; </p>
+ <p><input type="checkbox">  Formatar o novo disco com ext4 e montar em /mnt/teste_deu_boa. Este ponto de montagem deve ser iniciado após um reboot. </p>
  <p><input type="checkbox">  Criar um novo usuário chamado tux e um novo grupo chamado de linux. </p>
  <p><input type="checkbox">  Tux deve ser membro de linux. </p>
  <p><input type="checkbox">  Alterar o diretório para que apenas o grupo linux possa gravar e ler e o owner deve ser root. Nenhum outro usuário deve ter acesso algum a esta pasta. </p>
  <p><input type="checkbox">  Criar as pastas dir1, dir2 e dir3 dentro de /mnt/teste_deu_boa </p>
  <p><input type="checkbox">  Estas pastas além da permissão de grupo, devem ter como owner o user tux;  </p>
- <p><input type="checkbox">  Com o usuário tux crie um arquivo dentro de dir2 chamado de run.sh e neste arquivo deve constar o seguinte conteudo: </p>
- <p>			#!/bin/bash </p>
- <p>			echo "Uhuul deu boa" </p>
- <p>				# OBS: Teste o script para garantir que ele está rodando e trazendo a informação "Uhuul deu boa". </p>
- <p><input type="checkbox">  Dentro de dir3 criar um arquivo chamado de lista_dir, com uma saida do comando ls /mnt/teste_deu_boa; </p>
- <p>			OPCIONAL: Pesquisar sobre stdin, stdout, stderr. </p>
- <p><input type="checkbox">  Após ter rodado com sucesso remova o usuário tux do grupo linux. </p>
- <p>			OBS: Garanta e entenda o motivo que o usuário tux não consegue mais entrar em /mnt/teste_deu_boa; </p>
- <p>----------------------------------------------------------------------------------------------------</p>
+ <p><input type="checkbox">  Com o usuário tux crie um arquivo dentro de dir2 chamado de run.sh e neste arquivo deve constar o conteudo abaixo. OBS: Teste o script para garantir que ele está rodando e trazendo a informação "Uhuul deu boa".: </p>
+ <code><p>#!/bin/bash</p><p>echo "Uhuul deu boa"</p> </code>
+<textarea name="text" cols="25" rows="3">
+#!/bin/bash
+echo "Uhuul deu boa"
+</textarea>
+ <p><input type="checkbox">  Dentro de dir3 criar um arquivo chamado de lista_dir, com uma saida do comando ls /mnt/teste_deu_boa. OPCIONAL: Pesquisar sobre stdin, stdout, stderr. </p>
+ <p><input type="checkbox">  Após ter rodado com sucesso remova o usuário tux do grupo linux. OBS: Garanta e entenda o motivo que o usuário tux não consegue mais entrar em /mnt/teste_deu_boa; </p>
+</hr>
  </body>
 </html>
