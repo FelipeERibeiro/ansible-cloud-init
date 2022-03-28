fde87fd768f592713f2c13c3fa9d21340051dbe3
Felipe E. Ribeiro <75494201+felipe-e-ribeiro@users.noreply.github.com>
2022-03-27 22:42:03 -0300

    Update index.php

--- docker-compose/public_html/index.php
+++ docker-compose/public_html/index.php
@@ -1,6 +1,4 @@
 <!DOCTYPE html>
-<html>
-<head>
 <title>
 Server <?php print exec('cat /run/hostname_server | awk -F \- \'{print $2}\' '); ?>
 </title>
@@ -14,7 +12,9 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
 <p>Este ambiente possui um HA baseado em servidores rodando em nginx com um LB.</p>
 <p>Este &eacute; o servidor:  </p>
 <li><?php print exec('cat /run/hostname_server'); ?></li>
-<p>Linux - Sistema Operacional 01<p>
+<h3>Estudos Linux</h3>
+<details>
+  <summary>Semana 01</summary>
 <ol>
 <li> Estrutura de diretório</li>
 <ul>
@@ -53,8 +53,8 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
 <li>Entender diferença do elemento "x" em pastas e arquivos.            </li>
 </ul>
 <ol>
-<hr>
-<p>DESAFIO 01</p>
+<details>
+<summary>DESAFIO 01</summary>
  <p><input type="checkbox">  Criar um novo disco com ao menos 10 MB. OPCIONAL: Criar um lvm chamado lv_disco; </p>
  <p><input type="checkbox">  Formatar o novo disco com ext4 e montar em /mnt/teste_deu_boa. Este ponto de montagem deve ser iniciado após um reboot. </p>
  <p><input type="checkbox">  Criar um novo usuário chamado tux e um novo grupo chamado de linux. </p>
@@ -63,13 +63,57 @@ font-family: Tahoma, Verdana, Arial, sans-serif; }
  <p><input type="checkbox">  Criar as pastas dir1, dir2 e dir3 dentro de /mnt/teste_deu_boa </p>
  <p><input type="checkbox">  Estas pastas além da permissão de grupo, devem ter como owner o user tux;  </p>
  <p><input type="checkbox">  Com o usuário tux crie um arquivo dentro de dir2 chamado de run.sh e neste arquivo deve constar o conteudo abaixo. OBS: Teste o script para garantir que ele está rodando e trazendo a informação "Uhuul deu boa".: </p>
- <code><p>#!/bin/bash</p><p>echo "Uhuul deu boa"</p> </code>
 <textarea name="text" cols="25" rows="3">
 #!/bin/bash
 echo "Uhuul deu boa"
 </textarea>
  <p><input type="checkbox">  Dentro de dir3 criar um arquivo chamado de lista_dir, com uma saida do comando ls /mnt/teste_deu_boa. OPCIONAL: Pesquisar sobre stdin, stdout, stderr. </p>
  <p><input type="checkbox">  Após ter rodado com sucesso remova o usuário tux do grupo linux. OBS: Garanta e entenda o motivo que o usuário tux não consegue mais entrar em /mnt/teste_deu_boa; </p>
-</hr>
- </body>
-</html>
+</details>
+</details>
+
+
+<details>
+  <summary>Semana 02</summary>
+<ol>
+<li>Processos/PID</li><ul>
+<li>Entender como o linux organiza e identifica os processos;</li>
+<li>Entender como encontrar o PID sobre um processo especifico;</li>
+<li>Compreender como matar um processo;</li></ul>
+<li>Gerenciador de pacotes</li><ul>
+<li>Diferenciar arquivos .rpm e .deb e aonde são usados cada;</li>
+<li>Entender como utilizar os gerenciadores de pacote para:</li>
+<ul><li>Instalar determinado comando/serviço</li>
+<li>Pesquisar qual é o pacote de determinado comando</li>
+<li>Atualizar e voltar versão de determinado serviço/pacote</li></ul>
+<li>Compreender como listar os pacotes e verificar se já possui instalado determinado pacote/serviço.</li></ul>
+<li>Serviços</li><ul>
+<li>Parar/inicar serviços</li>
+<li>Analisar a porta que determinado serviço está utilizado (se o serviço possuir porta)</li>
+<li>Ativar/desativar serviços no boot</li>
+<li>Extra: Criar/personalizar um novo serviço com systemd.</li></ul>
+<li>Localizando</li><ul>
+<li>Como localizar o local de um arquivo que apenas possui o nome?</li>
+<li>Como localizar um arquivo que você possui apenas ideia do que esteja escrito nele ?</li></ul>
+<li>Análise de log</li><ul>
+<li>Uso do journalctl para localizar logs de serviços</li>
+<li>Identificar local padrão de logs do sistema operacional</li>
+<li>Extra: Entendimento do serviço rsyslog</li></ul>
+</ol>
+<details>
+<summary>DESAFIO 02</summary>
+<p><input type="checkbox"> Instalar o pacote httpd; </p>
+<p><input type="checkbox"> Iniciar o serviço (dexando ele ativo no boot); </p>
+<p><input type="checkbox"> Verifique qual porta que o serviço iniciou; </p>
+<p><input type="checkbox"> Realizar acesso no seu servidor e encontrar os logs de acesso; </p>
+<p><input type="checkbox"> Encontrar o arquivo de conf httpd.conf utilizando os comandos visto no topico 4; </p>
+<p><input type="checkbox"> Verifique quais PIDs estão atrelados para o serviço httpd. Realize um stop, valide novamente, e por fim, um start no serviço e compare os PIDs nestes 3 momentos; </p>
+<p><input type="checkbox"> Personalize a sua página web com um arquivo index.html com a informação: </p>
+<textarea name="text" cols="25" rows="3">
+Meu primeiro webserver, deu boa
+</textarea>
+</details>
+</details>
+
+
+</body>
